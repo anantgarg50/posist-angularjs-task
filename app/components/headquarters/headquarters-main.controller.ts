7d228177b34846b32336739d772ce4938ae69e0c
Anant Garg <anantgarg50@gmail.com>
2020-01-24 18:44:52 +0530

    Added headquarters controller

--- app/components/headquarters/headquarters-main.controller.ts
+++ app/components/headquarters/headquarters-main.controller.ts
@@ -1,5 +1,39 @@
-export class HeadquartersMainController {
+import angular from 'angular';
 
+interface Headquarter {
+  name: string,
+  location: string
+}
+
+export class HeadquartersMainController {
+  static $inject = ['headquartersService'];
+
+  public initialValue: Headquarter = {
+    name: undefined,
+    location: undefined
+  };
+  public headquarter: Headquarter = {
+    name: undefined,
+    location: undefined
+  };
+
+  constructor(private headquartersService: any) { }
+
+  createHeadquarter(headquarterForm: angular.IFormController) {
+    if (!headquarterForm.$valid) {
+      return;
+    }
+    console.log(this.headquarter);
+
+    this.resetForm(headquarterForm);
+  }
+
+  resetForm(headquarterForm: angular.IFormController) {
+    headquarterForm.$setPristine();
+    headquarterForm.$setUntouched();
+
+    this.headquarter = angular.copy(this.initialValue);
+  }
 };
 
 export default HeadquartersMainController;
