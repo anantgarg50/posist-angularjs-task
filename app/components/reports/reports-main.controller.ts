f4253bc9da6a28613f1ac5e0512d44b6c257029c
Anant Garg <anantgarg50@gmail.com>
2020-01-28 16:49:23 +0530

    DONE: reports

--- app/components/reports/reports-main.controller.ts
+++ app/components/reports/reports-main.controller.ts
@@ -1,3 +1,5 @@
+import { ReportsService } from '../../services/reports-service';
+
 interface ReportOption {
   id: string,
   name: string
@@ -6,15 +8,143 @@ interface ReportOption {
 export class ReportsMainController {
   static $inject = ['reportsService'];
 
-  private reportsService: any;
-
   public reportTitle: string;
   public reportOptions: ReportOption[];
   public selectedOption: ReportOption;
+  public tableColumns: string[];
+  public tableData: any[];
+  public rowCells: string[];
 
-  constructor(reportsService: any) {
-    this.reportsService = reportsService;
+  private columnConfig: any = {
+    headquartersRevenue: {
+      colNames: [
+        "S.N.",
+        "Headquarter Name",
+        "Location",
+        "Revenue"
+      ],
+      rowCells: [
+        "{{$index + 1 + '.'}}",
+        "{{data.name}}",
+        "{{data.location}}",
+        "{{data.revenue}}"
+      ]
+    },
+    branchesRevenue: {
+      colNames: [
+        "S.N.",
+        "Branch Name",
+        "Location",
+        "Headquarter",
+        "Revenue"
+      ],
+      rowCells: [
+        "{{$index + 1 + '.'}}",
+        "{{data.driverName}}",
+        "{{data.driverAge}}",
+        "{{data.numberOfBookings}}"
+      ]
+    },
+    rentalCarsRevenue: {
+      colNames: [
+        "S.N.",
+        "Registration No.",
+        "Make/Model",
+        "Operated By",
+        "Revenue"
+      ],
+      rowCells: [
+        "{{$index + 1 + '.'}}",
+        "{{data.driverName}}",
+        "{{data.driverAge}}",
+        "{{data.numberOfBookings}}"
+      ]
+    },
+    carDriversRevenue: {
+      colNames: ["S.N.", "Driver Name", "Age", "Revenue"],
+      rowCells: [
+        "{{$index + 1 + '.'}}",
+        "{{data.driverName}}",
+        "{{data.driverAge}}",
+        "{{data.numberOfBookings}}"
+      ]
+    },
+    carDriversHighestBookings: {
+      colNames: [
+        "S.N.",
+        "Driver Name",
+        "Age",
+        "No. of Bookings"
+      ],
+      rowCells: [
+        "{{$index + 1 + '.'}}",
+        "{{data.driverName}}",
+        "{{data.driverAge}}",
+        "{{data.numberOfBookings}}"
+      ]
+    },
+    rentalCarsHighestBookings: {
+      colNames: [
+        "S.N.",
+        "Registration No.",
+        "Make/Model",
+        "Operated By",
+        "No. of Bookings"
+      ],
+      rowCells: [
+        "{{$index + 1 + '.'}}",
+        "{{data.driverName}}",
+        "{{data.driverAge}}",
+        "{{data.numberOfBookings}}"
+      ]
+    },
+    branchesHighestBookings: {
+      colNames: [
+        "S.N.",
+        "Branch Name",
+        "Location",
+        "Headquarter",
+        "No. of Bookings"
+      ],
+      rowCells: [
+        "{{$index + 1 + '.'}}",
+        "{{data.driverName}}",
+        "{{data.driverAge}}",
+        "{{data.numberOfBookings}}"
+      ]
+    },
+    carDriversHighestKms: {
+      colNames: [
+        "S.N.",
+        "Driver Name",
+        "Age",
+        "KMs Driven"
+      ],
+      rowCells: [
+        "{{$index + 1 + '.'}}",
+        "{{data.driverName}}",
+        "{{data.driverAge}}",
+        "{{data.numberOfBookings}}"
+      ]
+    },
+    rentalCarsHighestKms: {
+      colNames: [
+        "S.N.",
+        "Registration No.",
+        "Make/Model",
+        "Operated By",
+        "KMs Driven"
+      ],
+      rowCells: [
+        "{{$index + 1 + '.'}}",
+        "{{data.driverName}}",
+        "{{data.driverAge}}",
+        "{{data.numberOfBookings}}"
+      ]
+    }
+  }
 
+  constructor(private reportsService: any) {
     this.reportOptions = [
       { id: "", name: "Select ..." },
       { id: "headquartersRevenue", name: "Headquarters Revenue" },
@@ -30,11 +160,53 @@ export class ReportsMainController {
 
     this.selectedOption = { id: "", name: "Select ..." };
 
-    this.updateReportTitle();
+    this.updateReportType();
   }
 
-  updateReportTitle() {
+  updateReportType() {
     this.reportTitle = this.selectedOption.id ? this.selectedOption.name : "Please select a report type first!";
+
+    if (this.selectedOption.id) {
+      this.tableColumns = this.columnConfig[this.selectedOption.id].colNames;
+      this.rowCells = this.columnConfig[this.selectedOption.id].rowCells;
+      this.populateReportData(this.selectedOption.id);
+    }
+  }
+
+  populateReportData(reportType: string) {
+    switch (reportType) {
+      case "headquartersRevenue":
+        this.tableData = this.reportsService.generateHeadquartersRevenueReport();
+        break;
+      case "branchesRevenue":
+        this.tableData = this.reportsService.generateBranchesRevenueReport();
+        break;
+      case "rentalCarsRevenue":
+        this.tableData = this.reportsService.generateRentalCarsRevenueReport();
+        break;
+      case "carDriversRevenue":
+        this.tableData = this.reportsService.generateCarDriversRevenueReport();
+        break;
+      case "carDriversHighestBookings":
+        this.tableData = this.reportsService.generateCarDriversHighestBookingsReport();
+        break;
+      case "rentalCarsHighestBookings":
+        this.tableData = this.reportsService.generateRentalCarsHighestBookingsReport();
+        break;
+      case "branchesHighestBookings":
+        this.tableData = this.reportsService.generateBranchesHighestBookingsReport();
+        break;
+      case "carDriversHighestKms":
+        this.tableData = this.reportsService.generateCarDriversHighestKmsReport();
+        break;
+      case "rentalCarsHighestKms":
+        this.tableData = this.reportsService.generateRentalCarsHighestKmsReport();
+        break;
+      default:
+        break;
+    }
+
+    console.log(this.tableData);
   }
 };
 
--- app/services/reports-service.ts
+++ app/services/reports-service.ts
@@ -2,18 +2,249 @@ import { HeadquartersService } from './headquarters-service';
 import { BranchesService } from './branches-service';
 import { CarsService } from './cars-service';
 import { DriversService } from './drivers-service';
-import { BookingsService } from './bookings-service';
+
+interface Booking {
+  _id?: string,
+  customerName: string,
+  carBooked: any,
+  pickupAddress: string,
+  startTime: string,
+  branch: any,
+  ratePerKilometer: number,
+  driverDetails: any,
+  hourlyRate: number,
+  endTime: string,
+  kmsTravelled: number,
+  billedAmount: number
+}
+
+interface Car {
+  _id?: string,
+  operatedBy: any,
+  drivenBy: any,
+  manufacturer: string,
+  model: string,
+  seatingCapacity: number,
+  ratePerKilometer: number,
+  hourlyRate: number,
+  carRegNumber: string,
+  bookings: any[],
+  currentlyBooked: boolean
+}
+
+interface Branch {
+  _id?: string,
+  name: string,
+  location: string,
+  headquarter: any,
+  cars: any[],
+  bookings: any[]
+}
+
+interface Driver {
+  _id?: string,
+  name: string,
+  age: number,
+  permanentAddress: string,
+  assignedCar: object,
+  bookings: any[]
+}
+
+interface Headquarter {
+  _id?: string,
+  name: string,
+  location: string,
+  branches: object[]
+}
 
 export class ReportsService {
-  static $inject = ['headquartersService', 'branchesService', 'carsService', 'driversService', 'bookingsService'];
+  static $inject = ['headquartersService', 'branchesService', 'carsService', 'driversService'];
 
   constructor(
     private headquartersService: HeadquartersService,
     private branchesService: BranchesService,
     private carsService: CarsService,
     private driversService: DriversService,
-    private bookingsService: BookingsService
   ) { }
+
+  generateHeadquartersRevenueReport() {
+    const headquarters = this.headquartersService.getList().map(
+      (headquarter: Headquarter) => {
+        return [
+          headquarter.name,
+          headquarter.location,
+          headquarter.branches.reduce((accumulator: number, current: Branch) => {
+            accumulator += this.branchesService.get(
+              current._id
+            ).bookings.reduce((accumulator: number, current: Booking) => {
+              accumulator += current.billedAmount;
+
+              return accumulator;
+            }, 0);
+
+            return accumulator;
+          }, 0)
+        ];
+      }
+    );
+
+    headquarters.sort((a: any, b: any) => b[2] - a[2]);
+
+    return headquarters;
+  }
+
+  generateBranchesRevenueReport() {
+    const branches = this.branchesService.getList().map(
+      (branch: Branch) => {
+        return [
+          branch.name,
+          branch.location,
+          `${branch.headquarter.name} (${branch.headquarter.location})`,
+          branch.bookings.reduce((accumulator: number, current: Booking) => {
+            accumulator += current.billedAmount;
+
+            return accumulator;
+          }, 0)
+        ];
+      }
+    );
+
+    branches.sort((a: any, b: any) => b[3] - a[3]);
+
+    return branches;
+  }
+
+  generateRentalCarsRevenueReport() {
+    const cars = this.carsService.getList().map(
+      (car: Car) => {
+        return [
+          car.carRegNumber,
+          `${car.manufacturer} ${car.model}`,
+          `${car.operatedBy.name} (${car.operatedBy.location})`,
+          car.bookings.reduce((accumulator: number, current: Booking) => {
+            accumulator += current.billedAmount;
+
+            return accumulator;
+          }, 0)
+        ];
+      }
+    );
+
+    cars.sort((a: any, b: any) => b[3] - a[3]);
+
+    return cars;
+  }
+
+  generateCarDriversRevenueReport() {
+    const drivers = this.driversService.getList().map(
+      (driver: Driver) => {
+        return [
+          driver.name,
+          driver.age,
+          driver.bookings.reduce((accumulator: number, current: Booking) => {
+            accumulator += current.billedAmount;
+
+            return accumulator;
+          }, 0)
+        ];
+      }
+    );
+
+    drivers.sort((a: any, b: any) => b[2] - a[2]);
+
+    return drivers;
+  }
+
+  generateCarDriversHighestBookingsReport() {
+    const drivers = this.driversService.getList().map(
+      (driver: Driver) => {
+        return [
+          driver.name,
+          driver.age,
+          driver.bookings.length
+        ];
+      }
+    );
+
+    drivers.sort((a: any, b: any) => b[2] - a[2]);
+
+    return drivers;
+  }
+
+  generateRentalCarsHighestBookingsReport() {
+    const cars = this.carsService.getList().map(
+      (car: Car) => {
+        return [
+          car.carRegNumber,
+          `${car.manufacturer} ${car.model}`,
+          `${car.operatedBy.name} (${car.operatedBy.location})`,
+          car.bookings.length
+        ];
+      }
+    );
+
+    cars.sort((a: any, b: any) => b[3] - a[3]);
+
+    return cars;
+  }
+
+  generateBranchesHighestBookingsReport() {
+    const branches = this.branchesService.getList().map(
+      (branch: Branch) => {
+        return [
+          branch.name,
+          branch.location,
+          `${branch.headquarter.name} (${branch.headquarter.location})`,
+          branch.bookings.length
+        ];
+      }
+    );
+
+    branches.sort((a: any, b: any) => b[3] - a[3]);
+
+    return branches;
+  }
+
+  generateCarDriversHighestKmsReport() {
+    const drivers = this.driversService.getList().map(
+      (driver: Driver) => {
+        return [
+          driver.name,
+          driver.age,
+          driver.bookings.reduce((accumulator: number, current: Booking) => {
+            accumulator += current.kmsTravelled;
+
+            return accumulator;
+          }, 0)
+        ];
+      }
+    );
+
+    drivers.sort((a: any, b: any) => b[2] - a[2]);
+
+    return drivers;
+  }
+
+  generateRentalCarsHighestKmsReport() {
+    const cars = this.carsService.getList().map(
+      (car: Car) => {
+        return [
+          car.carRegNumber,
+          `${car.manufacturer} ${car.model}`,
+          `${car.operatedBy.name} (${car.operatedBy.location})`,
+          car.bookings.reduce((accumulator: number, current: Booking) => {
+            accumulator += current.kmsTravelled;
+
+            return accumulator;
+          }, 0)
+        ];
+      }
+    );
+
+    cars.sort((a: any, b: any) => b[3] - a[3]);
+
+    return cars;
+  }
 };
 
 export default ReportsService;
