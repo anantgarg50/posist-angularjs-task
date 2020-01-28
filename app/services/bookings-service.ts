d51fb99c0cf4a2cdaeef1205ad5b06d2c6f35c44
Anant Garg <anantgarg50@gmail.com>
2020-01-28 15:20:51 +0530

    DONE: bookings

--- app/services/bookings-service.ts
+++ app/services/bookings-service.ts
@@ -14,7 +14,7 @@ export class BookingsService {
   }
 
   save(data: object) {
-    this.storageService.saveEntry(this.DB_NAME, data);
+    return this.storageService.saveEntry(this.DB_NAME, data);
   }
 
   update(_id: string, data: object) {
@@ -27,6 +27,34 @@ export class BookingsService {
 
   getList() {
     return this.storageService.getAll(this.DB_NAME);
+  }
+
+  calculateBillAmount(
+    startTime: string,
+    endTime: string,
+    ratePerKilometer: number,
+    hourlyRate: number,
+    kmsTravelled: number
+  ) {
+    const DAILY_TIME_ALLOTTED: number = 8 * 60 * 60 * 1000;
+    const DAILY_KMS_ALLOTTED: number = 250;
+
+    const timeDiff: number = Number(new Date(endTime)) - Number(new Date(startTime));
+    const daysBooked: number = Math.ceil(timeDiff / (24 * 60 * 60 * 1000));
+    const totalAllowedHours: number = daysBooked * DAILY_TIME_ALLOTTED;
+    const minChargeableKms: number = daysBooked * DAILY_KMS_ALLOTTED;
+
+    const chargeableKms: number =
+      kmsTravelled < minChargeableKms ? minChargeableKms : kmsTravelled;
+    const extraChargeableHours: number = Math.ceil(
+      (timeDiff - totalAllowedHours) / (60 * 60 * 1000)
+    );
+
+    let billAmount: number = chargeableKms * ratePerKilometer;
+    billAmount +=
+      extraChargeableHours > 0 ? extraChargeableHours * hourlyRate : 0;
+
+    return billAmount;
   }
 };
 
